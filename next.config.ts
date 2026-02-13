9676d417e732b9a25c755a3835794e2e1d62e9c2
rhflffk019-hue <rhflffk019@gmail.com>
2026-02-14 01:53:57 +0900

    sample update

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
         // 사용자가 브라우저에서 보는 주소
         source: '/sample-report',
         // 실제로 서버가 몰래 가져와서 보여줄 진짜 주소
-        destination: '/share/0afe6925-3f29-463f-b44a-2d75fe8958f0',
+        destination: '/share/9134187f-c375-4afb-ab58-4f7b0026f120',
       },
     ];
   },
